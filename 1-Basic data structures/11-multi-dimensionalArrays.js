fc63bab5e94999ea354e6b157c78a7cd31427e70
Persona743 <siyavashshahidi7@gmail.com>
2023-03-06 22:23:42 +0330

    js common challenges

--- 1-Basic data structures/11-multi-dimensionalArrays.js
+++
@@ -1,9 +0,0 @@
-let nestedArray = [
-    ['deep'],
-    [['deeper'], ['deeper']],
-    [[['deepest'], ['deepest']], [[['deepest-est?']]]],
-]; //deepest is level 4 and deepest-est? is level 5
-
-nestedArray[2][1][0][0][0] = 'deeper still'; //replace deepest-est?
-
-console.log(nestedArray[2][1][0][0][0]);
--- 1-Basic data structures/7-spreadCombine.js
+++
@@ -1,5 +0,0 @@
-// combine itmes with spread...
-
-let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
-let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander'];
-console.log(thatArray);
